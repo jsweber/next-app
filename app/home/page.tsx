59ffca7187a9efb416bec5594c255896d0a18be1
jsweber <951092973@qq.com>
2024-01-07 21:53:06 +0800

    chore: config

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,11 @@
-import { FC, ReactNode } from 'react';
 import { Metadata } from 'next';
 
-export interface PageProps {
-    children: ReactNode
-}
 // metadata是next中的关键词，固定
 export const metadata: Metadata = {
     title: 'home'
 }
 
-const Page: FC<PageProps> = () => {
+const Page = ({}) => {
 
     return (
         <h1>Hello, Home page!</h1>
