5c1a78eb44bcba674557b36c81b103ca025c178d
jsweber <951092973@qq.com>
2024-01-30 00:01:19 +0800

    feat: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { FC, ReactNode } from 'react';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import Template from "./template"
+
+export const metadata: Metadata = {
+    title: 'hijsweber.com',
+    description: 'Next web'
+};
 
 export interface RootLayoutProps {
     children: ReactNode;
@@ -8,11 +16,22 @@ export interface RootLayoutProps {
 export default function RootLayout({
     children
 }){
+    const pathname = usePathname();
+
+    console.log(pathname);
+
     return (
         <html lang="en">
             <body>
-                <div>root layout</div>
-                <Template key="home">{children}</Template>
+                <div id="app">
+                    <aside>
+                        <Link href="/home">home</Link>
+                    </aside>
+                    <div className="content">
+                        <header>header</header>
+                        <Template key="home">{children}</Template>
+                    </div>
+                </div>
             </body>
         </html>
     );
--- app/template.tsx
+++ app/template.tsx
@@ -1,3 +1,7 @@
+
+// template和layout的区别是：template在共享的组件之间会重新生成实例，对于针对某些导航需要特殊行为的页面，
+// 使用template会是更好的选择，对于useEffect和useState等也会在每次导航中重新执行
+// 使用场景：layout适合只在刚加载执行一些行为但是每次页面切换不执行的场景；template适合不管是初次加载还是切换都执行的场景。
 export default function Template({ children }: { children: React.ReactNode }) {
     return <div>{children}</div>
 }
